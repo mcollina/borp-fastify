91c4e7d4c6b4b4598eea6a314c354c3d359725a7
Matteo Collina <hello@matteocollina.com>
2023-12-19 11:17:37 +0100

    add param test
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,10 +1,21 @@
 import Fastify from 'fastify';
+import { Type, TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 
 export async function build () {
-  const app = Fastify();
+  const app = Fastify().withTypeProvider<TypeBoxTypeProvider>();
 
   app.get('/', async () => {
     return { hello: 'world' };
+  });
+
+  app.get('/user/:id', {
+    schema: {
+      params: Type.Object({
+        id: Type.String()
+      })
+    }
+  }, async (request) => {
+    return { id: request.params.id };
   });
 
   return app;
--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,7 +2,7 @@ import { test } from 'node:test';
 import { build } from '../lib/app.js';
 import { strictEqual, deepStrictEqual } from 'node:assert';
 
-test('GET /', async (t) => {
+test('GET /', async () => {
   const app = await build();
 
   const response = await app.inject({
@@ -13,4 +13,17 @@ test('GET /', async (t) => {
   strictEqual(response.statusCode, 200);
   strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
   deepStrictEqual(response.json(), { hello: 'world' });
-})
+});
+
+test('GET /user/:id', async () => {
+  const app = await build();
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/user/foo'
+  });
+
+  strictEqual(response.statusCode, 200);
+  strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
+  deepStrictEqual(response.json(), { id: 'foo' });
+});
